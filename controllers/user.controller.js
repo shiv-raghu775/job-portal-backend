140235752d80cfea19c7ad113d731275bfc0aa57
shiv-raghu775 <SHIVRAGHUWANSHI775@GMAIL.COM>
2026-08-22 17:46:19 +0530

    Remove secure flag from cookie in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -97,7 +97,7 @@ export const login = async (req , res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000, httpOnly: true,secure: true, sameSite :'none'}).json({
+        return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000, httpOnly: true, sameSite :'none'}).json({
             message: `welcome back ${user.fullname}`,
             user,
             success: true,
